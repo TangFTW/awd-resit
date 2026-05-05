0c30ac1b368daddd22f4b2c245edc87e0dd9362a
Dale Tang <ss9430881@gmail.com>
2026-05-05 09:25:33 +0000

    search adjust

--- hkpo-resit/src/app/search-post/search-post.ts
+++ hkpo-resit/src/app/search-post/search-post.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit  } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { PostRecord } from '../postrecord.model';
 import {FormGroup, FormBuilder, Validators, ReactiveFormsModule} from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -20,16 +20,18 @@ export class SearchPost implements OnInit {
   serverData!: Object | null;
   serverDataArr!: any;
   selectedPost!: PostRecord;
+  message: string = 'Search record here.'
 
   constructor(fb: FormBuilder , http: HttpClient) {
     this.http = http;
     // Form Bulider
     this.searchPostForm = fb.group({
-      'searchKey': ['', Validators.required]
+      'id': [''],
+      'districtEN': [''],
+      'dayOfWeekCode': [''],
+      'mobileCode': ['']
     });
-    this.deletePostForm = fb.group({
-      'id': ['', Validators.required]
-    });
+
 
   }
 
@@ -44,14 +46,36 @@ export class SearchPost implements OnInit {
   }
 
   getAllPosts(): void {
-    this.http.get('http://localhost:3001/mobliepost').subscribe({
+    this.serverData = null;
+    let url = 'http://localhost:3001/mobilepost';
+    console.log(url);
+    this.http.get(url).subscribe({
       next: (res) => {
         console.log("Data received:", res);
-        this.serverDataArr = res;
+        this.serverDataArr = (res as any).data;
       },
       error: (err) => {
-        // port crash handaler
+        // port crash handle
         console.log("Error fetching posts:", err);
+      }
+    });
+  }
+
+  onSubmit(formValue: any): void {
+    this.serverData = null;
+    let url = 'http://localhost:3001/mobilepost?';
+    if (formValue.id) { url += 'id=' + formValue.id + '&'; }
+    if (formValue.mobileCode) { url += 'mobileCode=' + formValue.mobileCode + '&'; }
+    if (formValue.dayOfWeekCode) {url += 'dayOfWeekCode=' + formValue.dayOfWeekCode + '&';}
+    if (formValue.districtEN) {url += 'districtEN=' + formValue.districtEN + '&';}
+    console.log(url);
+    this.http.get(url).subscribe({
+      next: (res) => {
+        console.log("Search results:", res);
+        this.serverDataArr = (res as any).data;
+      },
+      error: (err) => {
+        console.log("Error searching posts:", err);
       }
     });
   }
@@ -61,4 +85,4 @@ export class SearchPost implements OnInit {
     this.getAllPosts();
   }
 
-  }
+}
