f22538a64b287d38c7e2dd1a0677746160081780
Dale Tang <ss9430881@gmail.com>
2026-05-07 03:11:12 +0000

    empty protection

--- hkpo-resit/src/app/update-post/update-post.ts
+++ hkpo-resit/src/app/update-post/update-post.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {FormGroup, FormBuilder, Validators, ReactiveFormsModule} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 
@@ -34,36 +34,41 @@ export class UpdatePost {
     });
   }
 
-    updateRecord(formValue: any): void {
-      this.serverData = null;
-      let url = 'http://localhost:3001/mobilepost/' + formValue.id;
+  updateRecord(formValue: any): void {
+    this.serverData = null;
+    let url = 'http://localhost:3001/mobilepost/' + formValue.id;
 
-      console.log("Updating ID:...");
-      console.log("URL:", url);
-      // Empty field cleanup, prevent giving empty values to the db.
-      Object.keys(formValue).forEach(key => {
-        if (formValue[key] === '' || formValue[key] === null) {
-          delete formValue[key];
-        }
-      });
+    console.log("Updating ID:...");
+    console.log("URL:", url);
+    // Empty field cleanup, prevent giving empty values to the db.
+    Object.keys(formValue).forEach(key => {
+      if (formValue[key] === '' || formValue[key] === null) {
+        delete formValue[key];
+      }
+    });
 
-      this.http.put(url, formValue).subscribe({
-        next: (res) => {
-          console.log(res);
-          this.message = "Record " + formValue.id + " updated successfully!";
-        },
-
-
-        error: (err) => {
-          // port crash handaler
-          alert("Failed to update! Is the server running?");
-          console.log(err);
-        }
-      });
-
-
-
+    if (Object.keys(formValue).length === 1) {
+      this.message = "Please fill in at least one field to update.";
+      return;
     }
+
+    this.http.put(url, formValue).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.message = "Record " + formValue.id + " updated successfully!";
+      },
+
+
+      error: (err) => {
+        // port crash handaler
+        alert("Failed to update! Is the server running?");
+        console.log(err);
+      }
+    });
+
+
+
+  }
 
 
 }
